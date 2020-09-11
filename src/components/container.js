ebec8f3429642d5944ffc31d0a9dc67353b5e124
la-magbanua <la.magbanua18@gmail.com>
2020-09-11 10:30:42 +0800

    added left and right padding to container on smaller viewports

--- src/components/container.js
+++ src/components/container.js
@@ -4,6 +4,10 @@ import styled from 'styled-components'
 const StyledContainer = styled.div`
   max-width: 1200px;
   margin: 0 auto;
+
+  @media screen and (max-width: 420px) {
+    padding: 0 1.2rem;
+  }
 `
 
 export default function Container({ children, ...props }) {
